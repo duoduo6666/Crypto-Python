19d27da37d8648a466bdb8431a92d782c0b3ffb0
duoduo6666 <2326064185@qq.com>
2022-11-02 14:36:15 +0800

    要提交的变更： 	新文件：   .gitignore 	新文件：   src/C/standard.c 	修改：     src/Crypto/benchmark.py

---
+++ src/C/standard.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <limits.h>
+
+char SHA1_Computation(char M[64],unsigned int H[5]){
+    
+    return 0;
+}
+char* SHA1(char M[],unsigned int e){
+    unsigned int a = (e+9)/64;
+    if ((e+9)%64 > 0){
+        a++;}
+    unsigned char byte[a*64];
+    for (int i = 0; i < e; i++){
+        byte[i] = M[i];
+    }
+    printf("\n");
+    return M;
+}
+
+int main()
+{
+    char data[] = "abc";
+    unsigned int len = 63;
+    printf("%s\n",SHA1(data,5));
+    // printf("supports: %u\n",__builtin_cpu_supports("sse4.2"));
+    // printf("is: %u\n",__builtin_cpu_is("core2"));
+    // SHA1_Computation(data,len);
+    return 0;
+}
--- src/Crypto/benchmark.py
+++ src/Crypto/benchmark.py
@@ -1,16 +1,61 @@
-import time
 import __init__ as Crypto
+import standard
+import collision
+from signal import signal,SIGINT
+from random import randbytes
+from time import perf_counter
 
 class perf:
     def __init__(self):
-        self.time = time.perf_counter()
+        self.time = perf_counter()
     def __enter__(self):
         pass
     def __exit__(self,exc_type,exc_val,exc_tb):
-        print(time.perf_counter()-self.time)
+        print(perf_counter()-self.time)
 
-import collision
-with perf():
+class size():
+    base = {
+        0 : "",
+        1 : "K",
+        2 : "M",
+        3 : "G",
+        4 : "T",
+        5 : "P"
+    }
+    def __init__(self,size:int):
+        self.size = size
+    def __to_str(self) -> str:
+        t = self.size
+        i = 0.
+        while t > 128:
+            t /= 1024
+            i += 1
+        return str(round(t,2)) + self.base[i]
+    def __repr__(self):
+        return self.__to_str()
+    def __str__(self):
+        return self.__repr__()
+
+if __name__ == "__main__":
+    func = {
+        "SHA-1":standard.SHA1,
+    }
+    argv = {
+        "SHA-1":(randbytes(56)),
+    }
+    num = 1024
+    def _exit_(signum, frame):
+        print("\b\b  \b\b")
+        raise SystemExit
+    signal(SIGINT,_exit_)
+    while True:
+        time = perf_counter()
+        for _ in range(num):
+            standard.SHA1(randbytes(56))
+        print(f"\r        \r{size(1/(perf_counter()-time)*num)}H/s",end="")
+        
+
+# with perf():
     # for i in ((bytes(range(0x100)) * 0x100)[i:i+8] for i in range(0,0x2000,8)):
     #     Crypto.SHA1(i)
-    collision.SHA1(Crypto.SHA1(b"abc"),4,8,8)
+    # collision.SHA1(Crypto.SHA1(b"abc"),4,8,8)
